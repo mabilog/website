9d5ff1e861cd43fd7ca73d2409775b4bce8c01f6
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-07-13 12:55:30 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import React from 'react'
+
+
+import Navbar from './components/Navbar.jsx';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
