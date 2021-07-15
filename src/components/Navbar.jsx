dcb74127bc4903b341edeeec1ceea038e694afe7
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-07-15 15:13:19 -0400

    please continue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.scss';
 import {Person, Mail} from '@material-ui/icons'
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar active">
       <div className="wrapper">
         <div className="left">
           <a href='#home' className='logo'>mabilog.</a>
@@ -12,15 +12,21 @@ const Navbar = () => {
           </div>
         </div>
         <div className="right">
-          <div className="personal__ing">
-            <Person className='icon icon__person'/>
-            <span>(514)638-7661</span>
-            <Mail className='icon icon__mail'/>
-            <span>g.angelo.m@gmail.com</span>
+          <div className="personal__info">
+            <Person className='icon icon__person pInfo'/>
+            <span id="pInfo">(514)638-7661</span>
+            <Mail className='icon icon__mail pInfo'/>
+            <span id="pInfo">g.angelo.m@gmail.com</span>
           </div>
-          <a href="#resume" className="scroll-links">Resume</a>
-          <a href="#projects" className="scroll-links">Projects</a>
-          <a href="#contact" className="scroll-links">Contact</a>
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+
+            {/* <a href="#resume" className="scroll-links">Resume</a>
+            <a href="#projects" className="scroll-links">Projects</a>
+            <a href="#contact" className="scroll-links">Contact</a> */}
+          </div>
         </div>
       </div>
     </div>
