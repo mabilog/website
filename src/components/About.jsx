35a6053ebdf5edd0980968ea7bf1fdafff1452d8
Mabilog <g.angelo.m@gmail.com>
2021-09-27 19:38:22 -0400

    typos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
       <p>I am an aspiring web developer looking for new and exciting opportunities in the world of developers. </p>
       <p>My journey to becoming a web developer is a little all over the place.
       </p>
-      <p>It starts with me applying to <b>Computer Science Technology</b> in <b>Vanier College</b> and switching to a business program in <b>John Molson School of Business</b></p>.
+      <p>It starts with me applying to <b>Computer Science Technology</b> in <b>Vanier College</b> and switching to a business program in <b>John Molson School of Business</b>.</p>
       <p>Around mid 2019, I decided to go back to developing and learning how to code and program. Web developing was a natural place for me to explore and relearn coding principals that I took from my years in Vanier.</p>
-      <p>In the beginning of 2020, I enrolled with <b>Concordia Bootcamp</b> to help challenge my learning to the next level. </p>
+      <p>In the beginning of 2020, I enrolled in <b>Concordia Bootcamp</b> to help challenge my learning to the next level. </p>
       <p>When the COVID-19 pandemic hit, it forced me to steer in a different direction. I had to take a break from developing and help my family to weather out the storm until everything stablizes. </p>
       <p>After my hiatus from building websites, I got back to my initial goal to become a full fledged web developer! </p>
       <p>Being a web developer is my ultimate goal. There are still a lot of areas for growth but I know that through hard work and determination, I believe I can make my dream into reality! </p>
